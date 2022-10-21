9c1b92a37f02f62a039d19c77ccc0f3af07b2605
NahLima <naiarafx@gmail.com>
2022-10-21 13:22:59 -0300

    remove senha

--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-
+from enviroment import SECRET_KEY_DJANGO
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5zri+_w6p4(1bq*3*5b_&3noyju8i6fhs5$^jp511#*x$psk0='
-
+SECRET_KEY = SECRET_KEY_DJANGO
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
